920e501db13a6380419c0746c1ed0d41a2ad3bef
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-03-17 12:50:43 +0800

    remove business component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './login/login.component';
 import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { WarehouseRegComponent } from './warehouse-reg/warehouse-reg.component';
-import { BusinessScopeComponent } from './business-scope/business-scope.component';
 import { LandingRegistrationComponent } from './landing-registration/landing-registration.component';
 import { AuthGuard } from './guards/auth.guard';
 import { loginRedirectGuard } from './guards/login-redirect.guard';
@@ -17,7 +16,6 @@ export const routes: Routes = [
   { path: 'landing-registration', component: LandingRegistrationComponent},
   { path: 'registration-form', component: RegistrationFormComponent },
   { path: 'warehouse-reg', component: WarehouseRegComponent },
-  { path: 'business-scope', component: BusinessScopeComponent },
   { path: 'user-registration', component: RegistrationFormComponent, canActivate: [AuthGuard] },
   { path: 'warehouse-reg', component: WarehouseRegComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'landingpage-orig' }
