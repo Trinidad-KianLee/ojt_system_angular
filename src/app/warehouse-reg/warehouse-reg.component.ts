f9d99901a8cef0f7d6f69253ff8434fae504cc35
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-03-18 10:11:05 +0800

    fix header and url  pathing

--- src/app/warehouse-reg/warehouse-reg.component.ts
+++ src/app/warehouse-reg/warehouse-reg.component.ts
@@ -7,18 +7,29 @@ import { PocketBaseService } from '../services/pocketbase.service'; // or your A
   templateUrl: './warehouse-reg.component.html'
 })
 export class WarehouseRegComponent {
+  firstName: string = '';
   constructor(
-    private pbService: PocketBaseService, // or your AuthService
+    public pb: PocketBaseService,
     private router: Router
-  ) {}
+  ) {
+    const userData = this.pb.getUserData(); 
+    this.firstName = userData ? userData["firstName"] : ''; 
+  }
+
+  logout(){
+    this.pb.logout();
+    this.router.navigate(['login']);
+  }
 
   goToRegister() {
-    if (!this.pbService.isLoggedIn()) {
+    if (!this.pb.isLoggedIn()) {
       alert('You must log in first!');
       this.router.navigate(['/login']);
       return;
     }
     this.router.navigate(['/registration-form']);
   }
+  
+  
   
 }
