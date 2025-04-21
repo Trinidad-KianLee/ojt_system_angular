2fd0542f5cba5ec9a09bd262c5470efcc40cdfcd
Eliza Alimasa <alimasaxeliza@gmail.com>
2025-04-21 14:03:05 +0800

    add landing page and designs

---
+++ src/app/pages/user-dashboard/pslicense-landing/pslicense-landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-pslicense-landing',
+  imports: [RouterModule],
+  templateUrl: './pslicense-landing.component.html',
+  styleUrl: './pslicense-landing.component.css'
+})
+export class PslicenseLandingComponent {
+
+}
---
+++ src/app/pages/user-dashboard/soc-ccr-landing/soc-ccr-landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-soc-ccr-landing',
+  imports: [RouterModule],
+  templateUrl: './soc-ccr-landing.component.html',
+  styleUrl: './soc-ccr-landing.component.css'
+})
+export class SocCcrLandingComponent {
+
+}
