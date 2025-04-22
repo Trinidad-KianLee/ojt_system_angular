df9032cd7fc02372c75ab25b653d23a0baa8dc3d
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-04-22 16:45:03 +0800

    update favicon image

--- src/app/pages/user-dashboard/warehouse-registration/warehouse-registration.component.spec.ts
+++ src/app/pages/user-dashboard/warehouse-registration/warehouse-registration.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WarehouseRegistrationComponent } from './warehouse-registrationcomponent';
+import { WarehouseRegistrationComponent } from './warehouse-registration.component';
 
 describe('WarehouseRegistrationComponent', () => {
   let component: WarehouseRegistrationComponent;
